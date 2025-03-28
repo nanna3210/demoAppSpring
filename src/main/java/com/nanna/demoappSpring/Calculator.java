b28fd6959db507cfb919ae37663412c7f7a004a3
nanna <navkishoredas@gmail.com>
2025-03-28 09:42:28 +0530

    created a repo for testing

---
+++ src/main/java/com/nanna/demoappSpring/Calculator.java
@@ -0,0 +1,12 @@
+package com.nanna.demoappSpring;
+
+/**
+ * @copyright Reserved to Barclays Corporation
+ * All rights reserved. Unauthorized copying of this file, 
+ * via any medium, is strictly prohibited.
+ * 
+ * This file is part of the Barclays software system and 
+ * is intended for internal use only.
+ */
+public class Calculator {
+}
---
+++ src/test/java/com/nanna/demoappSpring/CalculatorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * @copyright Reserved to Barclays Corporation
+ * All rights reserved. Unauthorized copying of this file, 
+ * via any medium, is strictly prohibited.
+ * 
+ * This file is part of the Barclays software system and 
+ * is intended for internal use only.
+ */
+class CalculatorTest {
+  
+}
